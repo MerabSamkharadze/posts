b4b63b9eb459ba865db99b75b299882fb3e9f598
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-12 19:20:48 +0400

    add posts with its titles on post_container

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const post_contant = document.querySelector(".post_contant");
+
+fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+  .then((response) => {
+    if (!response.ok) {
+      throw response.status;
+    }
+    return response.json();
+  })
+  .then((posts) => {
+    const fragment = new DocumentFragment();
+    posts.forEach((element) => {
+      const postDiv = document.createElement("div");
+      postDiv.classList.add("post");
+      const h1 = document.createElement("h1");
+      h1.textContent = element.title;
+      const p = document.createElement("p");
+      p.textContent = element.body;
+      postDiv.appendChild(h1);
+      postDiv.appendChild(p);
+      fragment.appendChild(postDiv);
+    });
+    post_contant.appendChild(fragment);
+  })
+  .catch(() => {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Server Problem";
+    document.body.appendChild(h1);
+  });
